a0e5033e65bb8de1a75631fb2489e5e9743021a6
Claude Ihany <63082098+C-Iahny@users.noreply.github.com>
2023-10-27 20:02:37 +0200

    Main 02 (#3)
    * Add files via upload
    
    * contact update
    
    * contact update (#2)
    
    * Add files via upload (#1)
    
    * mksd home update 02
    
    * update
    
    * migrations
    
    * templates

--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Kontact, About, Impressum, Anmeldebogen
+from .views import Kontact, About, Impressum, Anmeldebogen, uber_uns, faq
 
 
 
@@ -8,6 +8,8 @@ urlpatterns = [
 	path('about/', About.as_view(), name="about"), 
 	path('impressum/', Impressum.as_view(), name="impressum"), 
 	path('anmeldebogen/', Anmeldebogen, name="anmeldebogen"), 
+	path('uber_uns/', uber_uns, name="uber_uns"), 
+	path('faq/', faq, name="faq"), 
 
 
 ]
